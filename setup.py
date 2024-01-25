edffd42b11ff8bdb0025556e09ccde084f871b23
Syirezz <111554400+KailUser@users.noreply.github.com>
2024-01-25 11:24:06 +0500

    0.1v of PyGram

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyGram',
+    version='0.0.1',
+    packages=find_packages(),
+    description='PyGram - tiny bot api for telegram',
+    author='Syirezz',
+    author_email='syirezz@icloud.com',
+    # url='',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
